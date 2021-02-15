4c21eee25bc426b9bbf797deb4f0cf2e20562433
Colten Appleby <colten96@gmail.com>
2021-02-15 00:59:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -8,7 +8,7 @@ def start_game(player1, player2)
   #binding.pry
 end
 
-def play_game(playe1, player2)
+def play_game(player1, player2)
   #binding.pry
   "#{player1} is better than #{player2}"
   #binding.pry
