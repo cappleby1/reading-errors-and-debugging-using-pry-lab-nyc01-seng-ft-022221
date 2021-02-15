66f356ea497f6ddd4e3f0d65519e1e04030a4b58
Colten Appleby <colten96@gmail.com>
2021-02-15 01:01:06 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -6,12 +6,15 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
+binding.pry
 
 def state_log(star_date)
+  binding.pry
   "Captain's Log, star date #{star_date}."
 end
 
 def engage
+  binding.pry
   puts state_log(date)
   date = generate_star_date
 end
