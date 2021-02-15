a7f729a7f48a58ba6b74cc3bba8be2fe9c9f273a
Colten Appleby <colten96@gmail.com>
2021-02-15 00:53:43 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -3,12 +3,12 @@
 require "pry"
 
 def start_game(player1, player2)
-  binding.pry
-  "Hello #{player1} & #{playar2}"
-  binding.pry
+  #binding.pry
+  "Hello #{player1} & #{player2}"
+  #binding.pry
 end
 
-def play_game(player2)
+def play_game(player, player2)
   binding.pry
   "#{player1} is better than #{player2}"
   binding.pry
